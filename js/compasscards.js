1523e19740d7554c9063610de415f530333c97ad
徐浩倫 <x2004514@gmail.com>
2022-12-19 22:26:12 +0800

    Initial commit

---
+++ js/compasscards.js
@@ -0,0 +1,9 @@
+
+var btn = document.getElementById("btn001");
+var dialog = document.getElementById("dialog001");
+
+btn.addEventListener("click", function(){
+    dialog.style.display="block";
+})
+
+
