fec9fa2e7b852162e4a7d2d69cd9e33965ab6108
徐浩倫 <x2004514@gmail.com>
2023-02-01 21:15:15 +0800

    選單試做

--- js/menu.js
+++ js/menu.js
@@ -3,11 +3,15 @@ var unhover = document.getElementById("unhover");
 var hover = document.getElementById("hover");
 
 function mouseUp(){
+    hover.style.display="none";
+    unhover.style.display="block";
+}
+
+function mouseDown(){
     unhover.style.display="none";
     hover.style.display="block";
 }
 
-function mouseDown(){
-    hover.style.display="none";
-    unhover.style.display="block";
+function Select(){
+    return false;
 }
