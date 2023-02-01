06052023d1f404a9684314b86f18c1d8ec31e2a6
徐浩倫 <x2004514@gmail.com>
2023-02-01 22:26:15 +0800

    touch test

--- js/menu.js
+++ js/menu.js
@@ -2,16 +2,23 @@ var menubutton = document.getElementsByClassName("menubutton");
 var unhover = document.getElementById("unhover");
 var hover = document.getElementById("hover");
 
-function mouseUp(){
-    hover.style.display="none";
-    unhover.style.display="block";
-}
-
 function mouseDown(){
     unhover.style.display="none";
     hover.style.display="block";
 }
 
-function Select(){
-    return false;
+function mouseUp(){
+    hover.style.display="none";
+    unhover.style.display="block";
 }
+
+menubutton.addEventListener("touchstart", function(){
+    unhover.style.display="none";
+    hover.style.display="block";
+})
+
+menubutton.addEventListener("touchend", function(){
+    hover.style.display="none";
+    unhover.style.display="block";
+})
+
