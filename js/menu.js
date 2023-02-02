8fe8187860846cd40146cbf2fb92bf97ddd46a3e
徐浩倫 <x2004514@gmail.com>
2023-02-03 03:34:21 +0800

    手機版選單

--- js/menu.js
+++ js/menu.js
@@ -1,20 +1,35 @@
-var menubutton = document.getElementsByClassName("menubutton");
+var menubutton = document.getElementById("menubutton");
 var unhover = document.getElementById("unhover");
 var hover = document.getElementById("hover");
+var mainmenu = document.getElementById("phone_mode_menu_bg");
+var closer = document.getElementById("menu_closer");
 
 function mouseDown(){
-    unhover.style.display="none";
-    hover.style.display="block";
+    unhover.style.display= "none";
+    hover.style.display= "block";
 }
 
 function mouseUp(){
-    hover.style.display="none";
-    unhover.style.display="block";
+    hover.style.display= "none";
+    unhover.style.display= "block";
+    mainmenu.style.display= "flex";
 }
 
 menubutton.addEventListener("click", function(){
-    unhover.style.display="none";
-    hover.style.display="block";
-    hover.style.display="none";
-    unhover.style.display="block";
+    mainmenu.style.display= "flex";
+})
+
+menubutton.addEventListener("touchstart", function(){
+    unhover.style.display= "none";
+    hover.style.display= "block";
+})
+
+menubutton.addEventListener("touchend", function(){
+    hover.style.display= "none";
+    unhover.style.display= "block";
+    mainmenu.style.display= "flex";
+})
+
+closer.addEventListener("click", function(){
+    mainmenu.style.display= "none";
 })
